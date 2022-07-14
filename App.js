4315f904e43592bdbcbe0d418b882d57a4d637a1
developerekene <developerekene@gmail.com>
2022-07-14 18:03:34 +0100

    Initial Setup with Notifee and RN-Firebase

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import notifee from '@notifee/react-native';
+import PushNotification from './components/notifee/PushNotification';
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+    <View style={{
+      flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    }}>
+      <PushNotification/>
+    </View>
+  );
+
+}
+
